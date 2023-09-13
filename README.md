483c5e01a7c7a90308a8799072529e2daa2bcd09
Shadowbot <benyshad@gmail.com>
2023-09-13 00:02:44 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# To-Do List Project
+
+This is a simple to-do list app built using React. It allows users to add and delete tasks they want to work on.
+
+# Installation
+To install the project and its dependencies, run the following command:
+```bash
+npm install
+```
+
+# Usage
+To start the app, run the following command
+```bash
+npm start
+```
+The app will be server on ```localhost:3000```
---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isVaild, setIsVaild] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (enteredValue.trim().length >= 0) {
+      setIsVaild(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsVaild(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isVaild ? 'invalid' : ''}`}>
+        <label>Today's Goals</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
